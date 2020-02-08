97b5cf5e1cf973743bf78ae3d7b3b848c7ea3e44
AlexanderJoel <jas.vargas@yavirac.edu.ec>
2020-02-07 19:31:02 -0500

    ducks

---
+++ interfaces/quack.js
@@ -0,0 +1,13 @@
+class QuackBehavior {
+    quack() {
+        console.log('quack');
+    }
+    noquack() {
+        console.log('no quack');
+    }
+    quick() {
+        console.log('quack quack');
+    }
+}
+
+module.exports = QuackBehavior;
